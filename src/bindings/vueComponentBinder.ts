b5ef49dbc911c722dee1e3c3dc21a48e1e365faf
Tuan Nguyen <tuanguye@microsoft.com>
2021-08-15 01:52:24 -0700

    Add Vue-binder

--- src/bindings/vueComponentBinder.ts
+++ src/bindings/vueComponentBinder.ts
@@ -5,23 +5,43 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file and at https://paperbits.io/license/mit.
  */
+import { ComponentBinder } from "@paperbits/common/editing/componentBinder";
+import { WidgetBinding } from "@paperbits/common/editing/widgetBinding";
 
- import { ComponentBinder } from "@paperbits/common/editing/componentBinder";
- import { WidgetBinding } from "@paperbits/common/editing/widgetBinding";
+export class VueComponentBinder implements ComponentBinder {
+    public init(element: Element, binding: WidgetBinding<any, any>): void {
+        let flowClassName;
 
- export class ReactComponentBinder implements ComponentBinder {
-     public init(element: Element, binding: WidgetBinding<any, any>): void {
-        //  const viewModelInstance = ...
-        //  binding.viewModel = viewModelInstance;
- 
-        //  if (binding.onCreate) {
-        //      binding.onCreate(viewModelInstance);
-        //  }
-     }
- 
-     public dispose(element: Element, binding: WidgetBinding<any, any>): void {
-         if (binding.onDispose) {
-             binding.onDispose();
-         }
-     }
- }
+        switch (binding.flow) {
+            case "block":
+                flowClassName = "block";
+                break;
+            case "inline":
+                flowClassName = "inline";
+                break;
+            case "none":
+                flowClassName = "placeholder";
+                break;
+            default:
+                console.warn(`Uknown component flow: ${binding.flow}`);
+        }
+
+        element.classList.add(flowClassName);
+        
+        const component = Reflect.getMetadata("paperbits-vue-component", binding.viewModelClass).component;
+
+        const viewModelInstance = new component().$mount(element);
+
+        binding.viewModel = viewModelInstance;
+
+        if (binding.onCreate) {
+            binding.onCreate(viewModelInstance);
+        }
+    }
+
+    public dispose(element: Element, binding: WidgetBinding<any, any>): void {
+        if (binding.onDispose) {
+            binding.onDispose();
+        }
+    }
+}
